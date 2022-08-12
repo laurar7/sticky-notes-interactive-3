311d5741cdc3d36a80b3b8b8f1c14dfcac64c468
Laura R <102334514+laurar7@users.noreply.github.com>
2022-08-12 19:07:05 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import Header from "./Header.js";
+import NotesList from "./NotesList.js";
+
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: Date.now(),
+        title: "",
+        description: "",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: ""
+  };
+
+  // Create a new note
+  addNote = () => {
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true
+    };
+    // add new note to an existing note in array
+    this.setState({ notes: [newNote, ...this.state.notes] });
+    //
+  };
+
+  onType = (editMeId, updatedKey, updatedValue) => {
+    //editMeId == Id of the note edited
+    // updatedKey == title or description field
+    // updatedValue == value of title or description
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
+  onSearch = (text) => {
+    const newSearchText = text.toLowerCase();
+    const updatedNotes = this.state.notes.map((note) => {
+      if (!newSearchText) {
+        note.doesMatchSearch = true;
+        return note;
+      } else {
+        const title = note.title.toLowerCase();
+        const description = note.description.toLowerCase();
+        const titleMatch = title.includes(newSearchText);
+        const descriptionMatch = description.includes(newSearchText);
+        const hasMatch = titleMatch || descriptionMatch;
+        note.doesMatchSearch = hasMatch;
+        return note;
+      }
+    });
+    this.setState({
+      notes: updatedNotes,
+      searchText: newSearchText
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header
+          onSearch={this.onSearch}
+          addNote={this.addNote}
+          searchText={this.state.searchText}
+        />
+        <NotesList onType={this.onType} notes={this.state.notes} />
+      </div>
+    );
+  }
+}
+
+export default App;
